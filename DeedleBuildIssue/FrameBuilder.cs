658bdf2d6f8421e069e91e6bc656cd162412e53a
Jonas Follesø <jonas@follesoe.no>
2022-06-17 11:01:26 +0200

    Initial commit

---
+++ DeedleBuildIssue/FrameBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Deedle;
+
+namespace DeedleBuildIssue
+{
+    public class FrameBuilder
+	{
+		public FrameBuilder()
+		{
+		}
+
+		// If I uncomment this method, the build hangs.
+		public Frame<DateTimeOffset, string> ToFrame()
+		{
+			return Frame.CreateEmpty<DateTimeOffset, string>();
+		}
+	}
+}
